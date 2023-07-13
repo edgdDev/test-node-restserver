d1ccfd78dcd2da6d5a89f11dc4c6a53cb7a682bd
Elvin Guzman <edgd900@gmail.com>
2023-07-13 10:17:19 -0400

    First commmit. My first REST API

---
+++ controllers/usuarios.js
@@ -0,0 +1,48 @@
+const { response, request } = require('express');
+
+const usuariosGet = (req = request, res = response) => {
+
+    const { q, nombre, apikey } = req.query;
+
+    res.json({
+        msg: 'get API - Controlador',
+        q,
+        nombre,
+        apikey
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+
+    const { id } = req.params;
+
+    res.json({
+        msg: 'put API - Controlador',
+        id
+    });
+}
+
+const usuariosPost = (req, res = response) => {
+
+    const { nombre, edad } = req.body;
+
+    res.json({
+        msg: 'post API - Controlador',
+        nombre,
+        edad
+    });
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg: 'delete API - Controlador'
+    });
+}
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete
+}
